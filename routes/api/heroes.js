5894d7eab4a69cdbb85fb2d8695c1f5fb4105fd0
Jing <jingkcheng@hotmail.com>
2019-07-22 19:02:10 -0400

    Added API routing

---
+++ routes/api/heroes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+const Heroes = require('../../models/Heroes');
+
+//@route    GET api/heroes
+//@desc     Get all heroes
+//@access   Public
+router.get('/', (req, res) => {
+    Heroes.find()
+        .sort({ class: 1 })
+        .then(heroes => res.json(heroes))
+});
+
+//@route    POST api/heroes
+//@desc     Create a hero
+//@access   Public
+router.post('/', (req, res) => {
+    try{
+        const newHero = new Heroes({
+            name: req.body.name,
+            class: req.body.class,
+            avatar: req.body.avatar,
+            type: req.body.type,
+            classification: req.body.classification,
+            rating: req.body.rating,
+            uwreliance: req.body.uwreliance
+        });
+        newHero.save().then(hero => res.json(hero));
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+module.exports = router;
