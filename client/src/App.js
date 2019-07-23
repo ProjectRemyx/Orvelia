95356a96bfaf8a4fae5709a8ef5d7f2d0abb43cc
Jing <jingkcheng@hotmail.com>
2019-07-23 16:19:13 -0400

    Added Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>test</h1>  
-    </div>
-  );
+import NavbarPage from './components/layout/Nav';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <NavbarPage/>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NavbarPage from './components/layout/Nav';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const nav = document.createElement('nav');
+  ReactDOM.render(<NavbarPage />, nav);
+  ReactDOM.unmountComponentAtNode(nav);
 });
