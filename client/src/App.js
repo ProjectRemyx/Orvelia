312ac513b40d248d22f8ebdc79924ed6fc8a5fbf
Jing <jingkcheng@hotmail.com>
2019-07-23 17:18:51 -0400

    Navbar changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-import './App.css';
+import './App.scss';
 
 import NavbarPage from './components/layout/Nav';
 
--- client/src/components/layout/Nav.js
+++ client/src/components/layout/Nav.js
@@ -3,19 +3,19 @@ import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNav
 MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon } from "mdbreact";
 
 class NavbarPage extends Component {
-state = {
-  isOpen: false
-};
-
-toggleCollapse = () => {
-  this.setState({ isOpen: !this.state.isOpen });
-}
-
-render() {
+    state = {
+        isOpen: false
+    };
+    
+    toggleCollapse = () => {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+    
+  render() {
   return (
-    <MDBNavbar color="default-color" dark expand="md">
+    <MDBNavbar color="transparent" light expand="md">
       <MDBNavbarBrand>
-        <strong className="white-text">Navbar</strong>
+        <img src={require('../../imgs/sock.jpg')} style={{  maxWidth: '30px' }} alt="Happy Sock"/>
       </MDBNavbarBrand>
       <MDBNavbarToggler onClick={this.toggleCollapse} />
       <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
@@ -24,46 +24,85 @@ render() {
             <MDBNavLink to="#!">Home</MDBNavLink>
           </MDBNavItem>
           <MDBNavItem>
-            <MDBNavLink to="#!">Features</MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink to="#!">Pricing</MDBNavLink>
+            <MDBDropdown>
+              <MDBDropdownToggle nav caret>
+                <div className="d-md-inline-block">Heroes</div>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu className="dropdown-default">
+                <MDBDropdownItem href="#!">All</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Knights</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Warriors</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Assassins</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Archers</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Mechanics</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Wizards</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Priests</MDBDropdownItem>                
+              </MDBDropdownMenu>
+            </MDBDropdown>
           </MDBNavItem>
           <MDBNavItem>
             <MDBDropdown>
               <MDBDropdownToggle nav caret>
-                <div className="d-none d-md-inline">Dropdown</div>
+                <div className="d-md-inline">Equipment</div>
               </MDBDropdownToggle>
               <MDBDropdownMenu className="dropdown-default">
-                <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                <MDBDropdownItem href="#!">UW</MDBDropdownItem>
+                <MDBDropdownItem href="#!">UT</MDBDropdownItem>
               </MDBDropdownMenu>
             </MDBDropdown>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBDropdown>
+              <MDBDropdownToggle nav caret>
+                <div className="d-md-inline">Raid</div>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu className="dropdown-default">
+                <MDBDropdownItem href="#!">Black Dragon</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Red Dragon</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Ice Dragon</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Poison Dragon</MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBDropdown>
+              <MDBDropdownToggle nav caret>
+                <div className="d-md-inline">GC</div>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu className="dropdown-default">
+                <MDBDropdownItem href="#!">Tyrfas</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Lakreil</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Velkazar</MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBDropdown>
+              <MDBDropdownToggle nav caret>
+                <div className="d-md-inline">WB</div>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu className="dropdown-default">
+                <MDBDropdownItem href="#!">Mountain Fortress</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Protianus</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Xanadus</MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink to="#!">PvP</MDBNavLink>
           </MDBNavItem>
         </MDBNavbarNav>
         <MDBNavbarNav right>
           <MDBNavItem>
-            <MDBNavLink className="waves-effect waves-light" to="#!">
-              <MDBIcon fab icon="twitter" />
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink className="waves-effect waves-light" to="#!">
-              <MDBIcon fab icon="google-plus-g" />
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBDropdown>
+            <MDBDropdown className="nav-contact">
               <MDBDropdownToggle nav caret>
-                <MDBIcon icon="user" />
+                <div className="d-md-inline">Contact</div>
               </MDBDropdownToggle>
-              <MDBDropdownMenu className="dropdown-default">
-                <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+              <MDBDropdownMenu className="dropdown-default" >
+                <MDBDropdownItem href="#!">Website</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Github</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Twitter</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Donate</MDBDropdownItem>
               </MDBDropdownMenu>
             </MDBDropdown>
           </MDBNavItem>
