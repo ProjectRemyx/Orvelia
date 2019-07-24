56a4ebffd6f7f84f9ba3a25d728074e9c6da6512
Jing <jingkcheng@hotmail.com>
2019-07-24 18:00:30 -0400

    Additional styling/prop work on overview section

--- client/src/components/layout/Index/Overview.js
+++ client/src/components/layout/Index/Overview.js
@@ -8,29 +8,29 @@ export default class Overview extends Component {
             {
                 id: 1,
                 title: 'Hero Builds',
-                img: 'link',
-                description: 'Lorem ipsum',
+                img: require('../../../imgs/builds.PNG'),
+                description: 'Not sure where to start in building your hero? We got you. Here you will find the most up to date builds and comprehensive guide to your favorite heroes',
                 button: 'More Details'
             },
             {
                 id: 2,
                 title: 'Equipment Info',
-                img: 'link',
-                description: 'Lorem ipsum',
+                img: require('../../../imgs/equipment.PNG'),
+                description: "Looking for information on specific hero UW and UTs? You've come to the right place. We provide all information including ideal UTs and tier list details for each one",
                 button: 'More Details'
             },
             {
                 id: 3,
-                title: 'World Boss Strats',
-                img: 'link',
-                description: 'Lorem ipsum',
+                title: 'World Boss',
+                img: require('../../../imgs/wb.PNG'),
+                description: "Need help planning your world boss teams? Unsure of where to start? This section will tell you all you need to know about team compositions and mechanics.",
                 button: 'More Details'
             },
             {
                 id: 4,
                 title: 'Conquest Guides',
-                img: 'link',
-                description: 'Lorem ipsum',
+                img: require('../../../imgs/gc.PNG'),
+                description: 'Want to bring your guild to new heights? We can help you do just that. This section contains information such as ideal team compositions and boss mechanics.',
                 button: 'More Details'
             }
         ]
@@ -39,13 +39,16 @@ export default class Overview extends Component {
         return (
             <div className="overview-section">
                 <div className="overview-section-intro">
-                    <h1><strong>Orvelia</strong></h1>
+                    <h1>Orvelia</h1>
                     <img src={require('../../../imgs/divider5.png')} style={{ maxWidth: '200px', marginTop: '-2.8em' }}alt="Divider"/>
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and 
-                        typesetting industry. Lorem Ipsum has been the industry's 
-                        standard dummy text ever since the 1500s, when an unknown 
-                        printer took a galley of type.
+                        Orvelia is a King's Raid toolkit / guide website made by Remyx (NA). 
+                        For inquiries you can reach me on Discord at Remyx#1327. The goal of
+                        Orvelia is to provide a centralized area for game information that
+                        the wiki site or other fan made sites do no contain. This application
+                        is made in MERN and uses it's own API. I am always open to feedback and 
+                        criticism. If you have any suggestions on how to improve Orvelia, please 
+                        do not hesitate to contact me.  
                     </p>
                 </div>
                 <div className="overview-section-details">
--- client/src/components/layout/Index/Section.js
+++ client/src/components/layout/Index/Section.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class Section extends Component {
     render() {
         const { img, title, description, button} = this.props.section
         return (
             <div className="section">
-                <img src= { img }/>
+                <img src= {img} alt="Hero Builds"/>
                 <h3>{ title }</h3>
                 <p>{ description }</p>
-                <button>{ button }</button>
+                <Link to="#">
+                    <button className="arrow">{ button }</button>
+                </Link>
             </div>
         )
     }
