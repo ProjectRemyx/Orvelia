3a1e04ee4e5b2950a16e8f0e1ceb93a1325e7d4a
Jing <jingkcheng@hotmail.com>
2019-08-10 18:50:42 -0400

    Getting data from API to Rating.js

--- client/src/components/layout/Heroes/Rating.js
+++ client/src/components/layout/Heroes/Rating.js
@@ -1,13 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Loading from '../Loading';
 
-class Rating extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Rating</h1>
-            </div>
-        );
+const Rating = props => {
+    const [name, setName] = useState({});
+    const [heroClass, setClass] = useState({});
+    const [type, setType] = useState({});
+    const [classification, setClassification] = useState({});
+    const [rating, setRating] = useState({});
+
+    useEffect(() => {
+        axios
+            .get(
+                `/api/heroes/${props.match.params.name}`
+                )
+                .then(res =>{
+                    let name = res.data.name;
+                    setName({ name });
+
+                    return axios.get(
+                        `/api/heroes/${props.match.params.name}`
+                    );
+                })
+                .then(res => {
+                    let heroClass = res.data.classification;
+                    setClass({ heroClass });
+                })
+                .then(res => {
+                    let type = res.data.type;
+                    setType({ type });
+                })
+                .then(res => {
+                    let classification = res.data.classification;
+                    setClassification({ classification });
+                })
+                .then(res => {
+                    let rating = res.data.rating;
+                    setRating({ rating });
+                })
+                .catch(err => console.log(err));
+    }, [props.match.params.name]);
+
+    const info = [name, heroClass, type, classification, rating];
+    for(let i = 0; i < info.length; i++)
+    {
+        if(info[i] === undefined || Object.keys(info[i]).length === 0)
+        {
+            return <Loading/>;
+        }
+        else
+        {
+            return(
+                <Link to="/Heroes">
+                    Back to hero list
+                </Link>
+            );
+        }
     }
-}
+};
 
 export default Rating;
